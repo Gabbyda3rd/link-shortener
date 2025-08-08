5a78ce8503b5a4a22bed32ff7d267a8a87b231d4
Gabbyda3rd <albertgabrielmalate@gmail.com>
2025-08-08 08:49:22 +0800

    added qr code image in the blade file

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -23,13 +23,12 @@
 <body class="bg-lightgray py-10 min-h-screen">
 
     <div class="max-w-xl mx-auto bg-white shadow-lg rounded-lg p-8 border-2 border-upmaroon">
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-red md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-red-600">URL Shortener</span></h1>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-red md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-red-600">Link Shortener</span></h1>
 
         <form action="{{ route('shorten') }}" method="POST" class="space-y-4">
             @csrf
             <input type="text" name="url" placeholder="Paste your Url Here" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-non focus:ring-2 focus:ring-upmaroon required">
             <button type="submit" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Shorten Url</button>
-
         </form>
         
         @if(isset($shortUrl))
@@ -43,6 +42,8 @@
             <div class="mt-6 text-center">
                 <p class="text-gray-700 mb-2">Scan QR Code:</p>
                 <img src="{{ asset('storage/'.$qrCodePath) }}" alt="QR Code" class="mx-auto h-40 w-40 border border-upmaroon p-1 bg-white">
+                {{-- <img src="{{ asset('storage/'.$qrCodePath) }}"> --}}
+
             </div>
         @endif
 
